a713636523655c1f9c16c6710188308c0c2b2a95
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2022-04-06 23:28:37 -0500

    Add router and pages and sidebar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,87 @@
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonIcon from "@mui/icons-material/Person";
+import StoreMallDirectoryIcon from "@mui/icons-material/StoreMallDirectory";
+import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
+import InsertChartOutlinedIcon from "@mui/icons-material/InsertChartOutlined";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SettingsSystemDaydreamIcon from "@mui/icons-material/SettingsSystemDaydream";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">Mario</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <PersonIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <StoreMallDirectoryIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <Inventory2OutlinedIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingOutlinedIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <InsertChartOutlinedIcon className="icon" />
+            <span>Status</span>
+          </li>
+          <li>
+            <NotificationsIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountBoxIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOptions"></div>
+        <div className="colorOptions"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
