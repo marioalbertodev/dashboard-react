1b96e4b3186aa26029cd734a74c64e9da81bcf6f
Mario Alberto Villagrana Hurtado <malberto1927@gmail.com>
2022-04-13 10:36:52 -0500

    Add new widget component

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,7 +1,108 @@
 import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 
-const Widget = () => {
-  return <div>Widget</div>;
+const Widget = ({ type }) => {
+  let data = {
+    title: "",
+    isMoney: false,
+    link: "",
+    icon: "",
+  };
+
+  const amount = 100;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonOutlineIcon
+            className="icon"
+            style={{
+              color: "crimson",
+              backgroundColor: "rgb(255,0,0,0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+          <ShoppingCartIcon
+            className="icon"
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgb(218,165,32,0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+          <MonetizationOnIcon
+            className="icon"
+            style={{
+              color: "green",
+              backgroundColor: "rgb(0,128,0,0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: (
+          <AccountBalanceWalletIcon
+            className="icon"
+            style={{
+              color: "purple",
+              backgroundColor: "rgb(128,0,128,0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    default:
+      break;
+  }
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"}
+          {amount}
+        </span>
+        <span className="link">{data.link}</span>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpIcon />
+          {diff} %
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
 };
 
 export default Widget;
